f2fc8a024f0b762c7a44d7820ad6c18f01fa07bf
Asmit Umrao <asmit@veersatech.com>
2025-07-16 14:22:56 +0530

    initialized the project

---
+++ src/main/java/model/User.java
@@ -0,0 +1,31 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles = new HashSet<>();   // e.g., "USER", "ADMIN","EVENT_CREATOR",
+
+}
---
+++ src/main/java/security/securityConfig.java
@@ -0,0 +1,15 @@
+package security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class securityConfig {
+
+//    @Autowired private JwtAuthenticationFilter jwtFilter;
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+
+}
